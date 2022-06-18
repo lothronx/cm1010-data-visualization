78b1e02a7db23081ffe1690dc776a22db6f76e4f
Lothron <yuewuxd@gmail.com>
2022-06-18 19:27:54 +0800

    -

--- waffle.js
+++ waffle.js
@@ -5,12 +5,12 @@ function Waffle(x, y, size, table, columnHeading) {
   const labels = table.getColumn(0).filter((value) => value != "");
   let percentage = table.getColumn(columnHeading);
   const colors = [
-    color(11, 50, 107), //blue
+    color(150, 110, 172), //purple
     color(245, 189, 66), //yellow
     color(176, 153, 119), //khaki
     color(130, 119, 117), //brown
     color(241, 199, 221), //pink
-    color(123, 203, 192), //cyan
+    color(230, 230, 230), //gray
   ];
 
   /* Categories is an array of objects made from the information above. ----------------------*/
@@ -22,34 +22,40 @@ function Waffle(x, y, size, table, columnHeading) {
     };
   });
 
-  /* Boxes is an array of all boxes. ---------------------------------------------------------*/
-  const boxSize = size / boxesEachLine;
-  const boxAmount = pow(boxesEachLine, 2); // The total amount of boxes is 100 in this case.
-  const boxes = Array.from(Array(boxAmount), () => new Box(x, y, boxSize, {})); // Make 100 empty boxes first.
-  assignValueToBoxes();
-
-  function assignValueToBoxes() {
-    boxes.forEach((box, i) => {
-      box.x = x + boxSize * (i % boxesEachLine);
-      box.y = y + boxSize * floor(i / boxesEachLine);
-    });
-
+  /* Boxes is a 2D array of all boxes. -------------------------------------------------------*/
+  let boxes = [];
+  function addBoxes() {
     let categoryIndex = 0;
     let boxesEachCategory = 0;
-    for (let i = 0; i < boxAmount; i++) {
-      if (boxesEachCategory == categories[categoryIndex].number) {
-        boxesEachCategory = 0;
-        categoryIndex++;
+    const boxSize = size / boxesEachLine;
+
+    for (let i = 0; i < boxesEachLine; i++) {
+      boxes.push([]);
+      for (let j = 0; j < boxesEachLine; j++) {
+        if (boxesEachCategory == categories[categoryIndex].number) {
+          boxesEachCategory = 0;
+          categoryIndex++;
+        }
+        boxes[i].push(
+          new Box(
+            x + j * boxSize,
+            y + i * boxSize,
+            boxSize,
+            categories[categoryIndex]
+          )
+        );
+        boxesEachCategory++;
       }
-      boxes[i].category = categories[categoryIndex];
-      boxesEachCategory++;
     }
   }
+  addBoxes();
 
   /* Draw the waffle chart -------------------------------------------------------------------*/
   this.draw = function () {
-    for (let i = 0; i < boxAmount; i++) {
-      boxes[i].draw();
+    for (let i = 0; i < boxesEachLine; i++) {
+      for (let j = 0; j < boxesEachLine; j++) {
+        boxes[i][j].draw();
+      }
     }
   };
 }
@@ -58,6 +64,6 @@ function Box(x, y, size, category) {
   this.draw = function () {
     noStroke();
     fill(category.color);
-    square(x, y, size, size / 4);
+    square(x, y, size, size / 2.5);
   };
 }
