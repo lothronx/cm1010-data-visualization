4f2e9580d07ca0c60152cfea8b49a20848abb4b4
Lothron <yuewuxd@gmail.com>
2022-06-18 18:43:07 +0800

    -

--- waffle.js
+++ waffle.js
@@ -3,7 +3,7 @@ function Waffle(x, y, size, table, columnHeading) {
 
   // Get information from the table to prepare for the categories.
   const labels = table.getColumn(0).filter((value) => value != "");
-  let data = table.getColumn(columnHeading);
+  let percentage = table.getColumn(columnHeading);
   const colors = [
     color(11, 50, 107), //blue
     color(245, 189, 66), //yellow
@@ -17,47 +17,41 @@ function Waffle(x, y, size, table, columnHeading) {
   let categories = labels.map((label, i) => {
     return {
       label: label,
-      number: round(data[i]),
+      number: round(percentage[i]),
       color: colors[i],
     };
   });
 
-  // Boxes is a 2D array of boxes
-  let boxes = [];
+  // Boxes is an array of all boxes.
   const boxSize = size / boxesEachLine;
-
-
-
-
-  let currentCategory = 0;
-  let currentCategoryBox = 0;
-  for (var i = 0; i < boxesEachLine; i++) {
-    boxes.push([]);
-    for (var j = 0; j < boxesEachLine; j++) {
-      if (currentCategoryBox == categories[currentCategory]) {
-        currentCategoryBox = 0;
-        currentCategory++;
-      }
-      boxes[i].push(
-        new Box(
-          x + j * boxSize,
-          y + i * boxSize,
-          boxSize,
-          categories[currentCategory]
-        )
-      );
-      currentCategoryBox++;
+  const boxAmount = pow(boxesEachLine, 2); // The total amount of boxes is 100 in this case.
+  // Make 100 empty boxes first.
+  const boxes = Array.from(Array(boxAmount), () => new Box(0, 0, boxSize, {}));
+  // Give value to each box.
+  boxes.forEach((box, i) => {
+    box.x = x + boxSize * (i % boxesEachLine);
+    box.y = y + boxSize * floor(i / boxesEachLine);
+  });
+  for (i = 0; i < boxAmount; i++) {
+    let categoryIndex = 0;
+    let boxesInCategory = 0;
+    if (boxesInCategory == categories[categoryIndex].number) {
+      boxesInCategory = 0;
+      categoryIndex++;
     }
+    boxes[i].category = categories[categoryIndex];
+    boxesInCategory++;
   }
 
+  console.log(boxes);
 
-
-  
   // Draw the waffle chart
   this.draw = function () {};
 }
 
 function Box(x, y, size, category) {
+  this.x = x;
+  this.y = y;
   this.category = category;
   this.draw = function () {
     noStroke();
